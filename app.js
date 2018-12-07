7d3ed61190e6f0faecbecb022b69977e18d0c6fd
Christian Collins <christiancollins743@gmail.com>
2018-12-07 04:37:10 +0000

    added mLab backend

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ var campgroundRoutes = require('./routes/campgrounds');
 var indexRoutes = require('./routes/index');
 
 
-mongoose.connect('mongodb://localhost/yelp_camp')
+// mongoose.connect('mongodb://localhost/yelp_camp')
+mongoose.connect('mongodb://christian:barfandstew22@ds141108.mlab.com:41108/yelpcamp555')
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
